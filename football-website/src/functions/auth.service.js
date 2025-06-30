8961532251ad48245fab83b010c97751fe02e595
Zkk0 <max.hasselwander@wiss-edu.ch>
2025-06-30 21:49:43 +0200

    finished Play-frontend-Impl with new Role Auth features

--- football-website/src/functions/auth.service.js
+++ football-website/src/functions/auth.service.js
@@ -26,10 +26,31 @@ const getJwtHeader = () => {
     let tok = JSON.parse(localStorage.getItem("user")).token
     return {headers: {Authorization: `Bearer ${tok}`}}
 }
+
+const getRole = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    return user?.roles?.[0] || user?.role || null;
+};
+
+const hasAnyRole = (targetRoles) => {
+  const user = getCurrentUser();
+  const rawRoles = user?.roles || [];
+
+  const roles = Array.isArray(rawRoles)
+    ? typeof rawRoles[0] === "string"
+      ? rawRoles
+      : rawRoles.map((r) => r.authority)
+    : [];
+
+  return targetRoles.some(role => roles.includes(role));
+};
+
 const AuthService = {
     login,
     logout,
     getJwtHeader,
     getCurrentUser,
+    getRole,
+    hasAnyRole
 }
 export default AuthService;
